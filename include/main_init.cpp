bf76593731fe5f19defaa13731975a208d3ffd8f
Petur Rosantsson <zashbot@gmail.com>
2011-09-09 15:34:20 -0700

    didn't save local changes

--- include/main_init.cpp
+++ include/main_init.cpp
@@ -2,23 +2,24 @@
 #include <SDL.h>
 #include <SDL_mixer.h>
 #include <iostream>
-
+using namespace std;
 SDL_Surface *screen;
-int main(int argc, char *argv[])
+
+int main(int argc, char *argv[]) //you need these because SDL redefines main() in this way.
 {
 	int InitVideo(SDL_HWSURFACE);
-	system("pause");
+	cin.get(); cin.get();
 	return 1;
 }
 
-int InitVideo(Uint32 flags = SDL_DOUBLEBUF | SDL_FULLSCREEN, int s_width=640, int s_height=480) 
+int InitVideo(Uint32 flags = SDL_DOUBLEBUF | SDL_FULLSCREEN, int s_width=640, int s_height=480) //will be changeable later using preload dialouge, do not assume these are static values when writing code
 {
   // Load SDL
-  if (SDL_Init(SDL_INIT_EVERYTHING | SDL_INIT_NOPARACHUTE) != 0) {
+  if (SDL_Init(SDL_INIT_EVERYTHING | SDL_INIT_NOPARACHUTE /* for debugging */) != 0) {
     fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
     return false;
   }
-  atexit(SDL_Quit);
+  atexit(SDL_Quit); //cleans up nicely
  
   screen = SDL_SetVideoMode(s_width, s_height, 32, flags);
   if (screen == NULL) {
