d7160644a3a31914ac9599134a7504a1cdb20930
Petur Rosantsson <zashbot@gmail.com>
2011-09-09 17:01:44 -0700

    added comments, dunno what happened

--- include/main_init.cpp
+++ include/main_init.cpp
@@ -25,17 +25,18 @@ int InitVideo(Uint32 flags = SDL_DOUBLEBUF | SDL_FULLSCREEN, int s_width=640, in
 
 int main(int argc, char *argv[]) //you need these because SDL redefines main() in this way.
 {
-	if (InitVideo(SDL_DOUBLEBUF | SDL_SWSURFACE) == 0) {cin.get();}
+	if (InitVideo(SDL_DOUBLEBUF | SDL_SWSURFACE) == 0) {cin.get(); return false;} //loads sdl and gives the user a chance to see the error message
+	
 	SDL_Surface *image; SDL_Surface *temp;
-	temp = SDL_LoadBMP("image.bmp"); if (temp == NULL) { printf("Unable to load bitmap: %s\n", SDL_GetError()); return 1; }
+	temp = SDL_LoadBMP("img/debug/image.bmp"); if (temp == NULL) { printf("Unable to load bitmap: %s\n", SDL_GetError()); return 1; }
 	image = SDL_DisplayFormat(temp); SDL_FreeSurface(temp);
-	SDL_Rect src, dest;
+	SDL_Rect src, dest; //creates 2 SDL rectangle structs
 	src.x = 0; src.y = 0; src.w = image->w; src.h = image->h;
 	dest.x = 100; dest.y = 100; dest.w = image->w; dest.h = image->h;
-	SDL_BlitSurface(image, &src, screen, &dest)	;
-	SDL_Flip(screen);
+	SDL_BlitSurface(image, &src, screen, &dest)	;  //loads the image onto the backbuffer
+	SDL_Flip(screen); //flips the backbuffer to the screen, therefore displaying the image
+	cout << "Image displayed \n";
 	SDL_Delay(2500);
-	cin.get(); cin.get();
 	return 1;
 }
 
