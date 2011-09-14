68d33da090439aa9dd76cc34a8ed54a7b8629d24
Petur Rosantsson <zashbot@gmail.com>
2011-09-14 16:27:44 -0700

    75% done with base entity class

--- include/entity.cpp
+++ include/entity.cpp
@@ -1,8 +1,13 @@
 #include "entity.h"
 
 
-entity::entity(void)
+entity::entity(int h, int w, int img_p, bool m_e)
 {
+	height=h;
+	width=w;
+	image_path=img_p;
+	motionEnabled=m_e;
+	pos_x = pos_y = vel_x = vel_y = 0;
 }
 
 
